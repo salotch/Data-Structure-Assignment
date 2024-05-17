923d924dd8babb57c1f2bc926d68e77c7dae63d2
Suhaila419 <163913554+Suhaila419@users.noreply.github.com>
2024-05-17 20:29:33 +0000

    Add files via upload

--- BST.cpp
+++ BST.cpp
@@ -23,6 +23,9 @@ public:
     bool operator==(Item &other){
         return ((this->itemName == other.itemName )&& (this->category == other.category) &&( this->price == other.price));
     }
+    bool operator<(Item &other){
+         return(this->itemName<other.itemName);
+    }
     bool operator>(Item &other){
         return (this->price > other.price);
     }
@@ -148,7 +151,7 @@ class BST{
     ~BST(){
         Clear(root);
     }
-     void AddItem(Item item)
+     void AddItemPrice(Item item)
     {
     TreeNode *current;
     TreeNode *trailCurrent;
@@ -180,7 +183,7 @@ class BST{
           trailCurrent->right = newNode;
    }
 }
-    bool search(Item &item){
+    bool searchPrice(Item &item){
         TreeNode *current = root;
         while (current !=  NULL) {
                 //cout << "Comparing with item: " << current->item.getName() << ", " << current->item.getCategory() << ", " << current->item.getPrice() << endl;
@@ -195,7 +198,7 @@ class BST{
         return false;
     }
 
-    void deleteItem(Item &item) {
+    void deleteItemPrice(Item &item) {
         TreeNode *current = root;
         TreeNode *trailCurrent =NULL;
         bool found = false;
@@ -228,37 +231,162 @@ class BST{
                 deleteFromTree(trailCurrent->right);
         }
     }
-};
+    void AddItemName(Item item)
+    {
+    TreeNode *current;
+    TreeNode *trailCurrent;
+    TreeNode *newNode;
+    newNode = new TreeNode;
+    assert(newNode != NULL);
+    newNode->item = item;
+    newNode->left = NULL;
+    newNode->right = NULL;
 
-int main(){
+    if(root == NULL)
+       root = newNode;
+    else
+    {
+       current = root;
+
+       while(current != NULL)
+       {
+           trailCurrent = current;
+              if(current->item < item)
+                 current = current->right;
+              else
+                 current = current->left;
+       }
+
+       if(trailCurrent->item < item)
+          trailCurrent->right = newNode;
+       else
+          trailCurrent->left = newNode;
+   }
+}
+    bool searchName(Item &item){
+        TreeNode *current = root;
+        while (current !=  NULL) {
+                //cout << "Comparing with item: " << current->item.getName() << ", " << current->item.getCategory() << ", " << current->item.getPrice() << endl;
+            if (current->item==item){
+                return true;
+            }
+            else if (current->item < item)
+                current = current->right;
+            else
+                current = current->left;
+        }
+        return false;
+    }
+
+    void deleteItemName(Item &item) {
+        TreeNode *current = root;
+        TreeNode *trailCurrent =NULL;
+        bool found = false;
+
+        while (current != NULL && !found) {
+                //cout << "Comparing with item: " << current->item.getName() << ", " << current->item.getCategory() << ", " << current->item.getPrice() << endl;
+            if (current->item==item){
+                found = true;break;
+            }
+            else {
+                trailCurrent = current;
+                if (current->item < item)
+                    current = current->right;
+                else
+                    current = current->left;
+            }
+        }
+
+        if (current == NULL) {
+            cout << "The delete item is not in the tree.\n";
+            return;
+        }
+
+        if (found) {
+            if (current == root)
+                deleteFromTree(root);
+            else if (trailCurrent->item < item)
+                deleteFromTree(trailCurrent->right);
+            else
+                deleteFromTree(trailCurrent->left);
+        }
+    }
+};
+void printPrice(){
     vector<Item>it=ReadItem();
     BST b;
-    /*for(auto& i:it){
-         cout<<i.getName()<<i.getCategory()<<i.getPrice() << endl;
-    }*/
+    //for(auto& i:it){
+      //   cout<<i.getName()<<i.getCategory()<<i.getPrice() << endl;
+    //}
     for (auto &item : it) {
-        b.AddItem(item);
+        b.AddItemPrice(item);
     }
-    /*Tuna
-      meat
-      90
- */
+    //Tuna
+      //meat
+      //90
+
  cout<<" * Print tree Inorder *\n";
     b.inorderTraversal();
     cout<<"***********************\n";
  Item itemDelete(" apples"," fruit",66);
-    if(b.search(itemDelete))cout<<"Yes,the Item is Found\n";
+    if(b.searchPrice(itemDelete))cout<<"Yes,the Item is Found\n";
     else cout<<"No,The Item is not Found\n";
     cout << "Deleting item:\n";
     itemDelete.print();
-    b.deleteItem(itemDelete);
+    b.deleteItemPrice(itemDelete);
     if(b.isEmpty())cout<<"YES, Tree IS Empty.\n";
     else cout<<"NO, Tree Have Item. \n";
+    cout<<"***********************\n";
     cout<<" * Print tree Inorder *\n";
     b.inorderTraversal();
+    cout<<"***********************\n";
     cout<<"\n * Print tree Preorder *\n";
     b.preorderTraversal();
+    cout<<"***********************\n";
     cout<<"\n * Print tree Postorder *\n";
     b.postorderTraversal();
+}
+void printName(){
+    vector<Item>it=ReadItem();
+    BST b;
+    //for(auto& i:it){
+      //   cout<<i.getName()<<i.getCategory()<<i.getPrice() << endl;
+    //}
+    for (auto &item : it) {
+        b.AddItemName(item);
+    }
+    //Tuna
+      //meat
+      //90
+
+ cout<<" * Print tree Inorder *\n";
+    b.inorderTraversal();
+    cout<<"***********************\n";
+ Item itemDelete(" Tuna"," meat",90);
+    if(b.searchName(itemDelete))cout<<"Yes,the Item is Found\n";
+    else cout<<"No,The Item is not Found\n";
+    cout << "Deleting item:\n";
+    itemDelete.print();
+    b.deleteItemName(itemDelete);
+    if(b.isEmpty())cout<<"YES, Tree IS Empty.\n";
+    else cout<<"NO, Tree Have Item. \n";
+    cout<<"***********************\n";
+    cout<<" * Print tree Inorder *\n";
+    b.inorderTraversal();
+    cout<<"***********************\n";
+    cout<<"\n * Print tree Preorder *\n";
+    b.preorderTraversal();
+    cout<<"***********************\n";
+    cout<<"\n * Print tree Postorder *\n";
+    b.postorderTraversal();
+}
+int main(){
+    cout<<"\n*********Price*********\n";
+    printPrice();
+    cout<<"\n*********Name*********\n";
+    printName();
     return 0;
 }
+
+
+
